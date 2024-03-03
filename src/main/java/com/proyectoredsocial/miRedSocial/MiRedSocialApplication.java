96136252d85f6ce176bf2d47268d19e4fcfe70ef
Ricardo Fernández Guzmán <ricardofg2000@gmail.comm>
2024-03-03 16:20:00 +0100

    Primer Commit con proyecto en Blanco

---
+++ src/main/java/com/proyectoredsocial/miRedSocial/MiRedSocialApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoredsocial.miRedSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiRedSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiRedSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoredsocial/miRedSocial/MiRedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoredsocial.miRedSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
